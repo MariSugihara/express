db0273e074249bf105f004424fa1e281826407d0
mari sugihara <mari0510@globe.ocn.ne.jp>
2025-07-14 15:40:30 +0900

    add cors middleware and update dependencies

--- routes/cat.js
+++ routes/cat.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios'); // requestの代わりにaxiosを使用
+const cors = require('cors'); // corsミドルウェアを追加
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get('/', async (req, res) => {
     try {
--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "**********";
+const uri = "mongodb+srv://MariSugihara:MDB-marisu0510@test.elyu8ee.mongodb.net/?retryWrites=true&w=majority&appName=test";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
